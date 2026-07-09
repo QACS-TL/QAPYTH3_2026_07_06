e4038b6780076bd1c4ed121052336c0191df133e
PBehague <114167055+QACS-TL@users.noreply.github.com>
2026-07-09 14:12:13 +0100

    Add files via upload

--- Zoo/animal.py
+++ Zoo/animal.py
@@ -1,17 +1,29 @@
 class Animal:
+    _limb_count:int = 0
 
-    def __init__(self, name:str="Anon", limb_count:int=4, colour:str="Black", energy:int=50):
+    def __init__(self, name:str="Anon", limb_count:int=4, colour:str="Black"):
         self.name = name
-        self.limb_count = limb_count
+        self._limb_count = limb_count
         self.colour = colour
-        self.energy = energy
+        self._energy = 50
+
+    def get_limb_count(self)->int:
+        return self._limb_count
+
+    def set_limb_count(self, value:int)->None:
+        if value < 0:
+            value = 0
+        self._limb_count = value
+
+    limb_count:int = property(get_limb_count, set_limb_count)
 
     def eat(self, food:str):
-        self.energy += 5
+        self._energy += 5
         return (f"I'm an {self.colour} animal called {self.name} "
-                f"using some of my {self.limb_count} limbs to eat {food}")
+                f"using some of my {self._limb_count} limbs to eat {food}")
 
     def move(self, direction:str):
-        self.energy -= 1
+        self._energy -= 1
+
         return (f"I'm an {self.colour} animal called {self.name} "
-                f"using some of my {self.limb_count} limbs to move {direction}")
+                f"using some of my {self._limb_count} limbs to move {direction}")
--- Zoo/main.py
+++ Zoo/main.py
@@ -2,19 +2,22 @@ from animal import Animal
 
 def main():
     ani1 = Animal()
-    ani2 = Animal("Rover", 2, "red", 60)
+    ani2 = Animal("Rover", 2, "red")
     # ani2.name = "Rover"
-    # ani2.limb_count = 2
+    ani2.limb_count = -1
+    #ani2.set_limb_count(-2)
+    #print(f"ani2's limb count is {ani2.get_limb_count()}")
+    print(f"ani2's limb count is {ani2.limb_count}")
     # ani2.colour = "red"
     # ani2.energy = 60
 
     print(ani1.move("North"))
     print(ani1.eat("cake"))
-    print(f"my energy level is {ani1.energy}")
+    #print(f"my energy level is {ani1.energy}")
 
     print(ani2.move("South"))
     print(ani2.eat("banana"))
-    print(f"my energy level is {ani2.energy}")
+    #print(f"my energy level is {ani2.energy}")
 
 
 if __name__ == '__main__':
